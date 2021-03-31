e4151ee0294a00c76c0c5cb1e94160500b82690e
Rems08 <56648191+Rems08@users.noreply.github.com>
2021-03-31 16:41:21 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,174 @@
+import json
+# on importe le module discord.py
+import discord
+import requests
+from random import*
+from discord.utils import get
+# ajouter un composant de discord.py
+from discord.ext import commands
+global piedsTeenWolf #Initialisation d'une variable globale qui va compter à partir de cette dernière
+piedsTeenWolf = 83
+global administrateur
+administrateur = "<@&596072853116420112>" #Identifiant du grade admin sur Cyber-Sport
+# créer le bot
+bot = commands.Bot(command_prefix='!')
+client = discord.Client()
+
+# détecter quand le bot est pret ("allumé")
+@bot.event
+async def on_ready():
+    print("Bot pret")
+    await bot.change_presence(status=discord.Status.online,
+            activity=discord.Game("Rems est le plus beau"))
+            
+# créer la commande !regles
+@bot.command()
+async def regles(ctx):
+    '''Cette commande permet d'afficher les règles du serveur Cyber-Sport'''
+    await ctx.send("Les règles:\n1. Respecter le prophète et tous les adhérants du discord\n2. Pas de double compte si vous n'en avez pas l'autorisation\n3. Pas de spam\n4. Si vous rencontrez un problème contactez un @Admin")
+
+#Créer la commande !ticket
+@bot.command()
+async def ticket(ctx, *message):
+    '''Envoie une un ticket au service d'administration du serveur: faites !ticket (votre message)'''
+    channel = discord.utils.get(ctx.guild.channels, name="❔ticket-administrateur❔")
+    channel_id = channel.id
+    auteur = ctx.author.mention
+    if channel_id == 826437578701144114:
+        await ctx.message.delete() #Supprime le message de la personne ayant rentré la commande
+        txt = ' '.join(message) #Permet de mettre un espace entre chaque tuple de la variable message
+        salon = discord.utils.get(ctx.guild.channels, name="chat-admins") #Initialise la variable salon sur le nom d'un salon dans name
+        await salon.send(f"{administrateur}{auteur} **a envoyé le ticket suivant:** {txt}")
+
+
+@bot.command() #Crée la commande !clash pour clasher un utilisateur random
+async def clash(ctx, nouveau_membre: discord.Member):
+    '''Permet de clasher un membre du discord en le mentionnant avec !clash @LePseudo'''
+    pseudo = nouveau_membre.mention
+    phraseDeClash = [f"Des fois je me sens super con mais quand je vois {pseudo} ça va mieux", f"L'intelligence chez {pseudo} c'est rare comme un 31 février", f"{pseudo} t’as pas léché un téton depuis que ta mère a stoppé de te donner le sein.", f"Chuis un robot et j'ai plus de talent que {pseudo} !", f"{pseudo} était mon plan A, j'etais ton plan B, tu croyais que ça allait finir en plan Q mais tu t plan T", f"J'ai alzheimer mais au moins je suis sûr de ne jamais oublier à quel point {pseudo} est complètement débile.", f"La place de la femme c'est à la cuisine mais la place de {pseudo} c'est loin de moi !"]
+    await ctx.send(phraseDeClash[randint(0, len(phraseDeClash) - 1)])
+
+@bot.command()
+async def disquettePrive(ctx, member: discord.Member):
+    '''Envoie une petite disquette à votre âme soeur grâce à la commande !disquettePrive @LePseudo'''
+    phraseDeDisquette = [f"Dans le royaume de mon cœur, c'est toi la princesse.", f"Moi sans toi, c'est comme un océan sans eau.", f" On s'est déjà vu quelque part, non? Tu ressembles énormément à ma prochaine petite copine.", f"Si un jour ton cœur ne bat plus, je te donnerai le mien, car sans toi, il me sert à rien.", f"Si tu veux savoir pourquoi je te suis, c'est parce que mon père m'a toujours dit de poursuivre mes rêves.", f"Tu dois être fatiguée à force de trotter dans ma tête toute la journée.", f"Si à chaque fois que je pensais à toi, il poussait une fleur, alors la Terre serait un immense jardin de roses.", f"Moi sans toi, c'est comme Roméo sans Juliette.", f"J'aimerais être une feuille morte devant  ta porte pour que tu m'écrases par ta beauté à chaque fois que  tu sors de chez toi.", f"Tu n'as pas eu mal quand tu es tombée du ciel mon ange ?", f" En temps normal je suis un très bon nageur, mais là je ne comprends pas je me noie dans tes yeux.", f"Si j'étais un juge, je nous obligerai à nous aimer à perpétuité.", f"Ton père travaille chez Nintendo si j’en crois ton corps de DS", f"Dommage que tu ne sois pas mon petit orteil par ce que je t'aurais bien démonté sur tous les coins de meuble !", f"Si tu cherches le chemin du bonheur je veux bien t'avancer de 22 cm ^^", f"Hey mademoiselle vous êtes tellement belle qu'il n'y a plus besoin de virgule !", f"Tu es plus que mon bonheur, tu es toute ma vie. Plus tu es heureux, plus je le suis ; ton bonheur fait mon bonheur. Si j'étais une larme je naîtrai de tes yeux pour caresser ta joue, et mourir sur tes lèvres. Grâce à toi, tous mes rêves d'amour, même les plus insensés, sont devenus une réalité."]
+    await member.send(phraseDeDisquette[randint(0, len(phraseDeDisquette) - 1)])
+    await member.send(f"Ce petit message vient de {ctx.author.mention}")
+
+# créer la commande !bienvenue @pseudo
+@bot.command()
+async def bienvenue(ctx, nouveau_membre: discord.Member):
+    '''Souhaitez la bienvenue à un membre du discord grâce à la commande !bienvenue @LePseudo'''
+
+    # recupere le nom
+    pseudo = nouveau_membre.mention
+    # executer le message de bienvenue
+    await ctx.send(f"Bienvenue à {pseudo} sur le serveur discord ! N'oublie pas de faire !regles")
+
+@bot.command()
+async def p(ctx):
+    '''Commande inutile pour le moment'''
+    global piedsTeenWolf
+    await ctx.send(piedsTeenWolf + 1)
+    await ctx.message.delete()
+    piedsTeenWolf = piedsTeenWolf + 1
+
+# verifier l'erreur
+@bienvenue.error
+async def on_command_error(ctx, error):
+    # detecter cette erreur
+    if isinstance(error, commands.MissingRequiredArgument):
+        # envoyer un message
+        await ctx.send("Tu dois faire !bienvenue @pseudo")
+
+def processString(txt):
+  specialChars = '{\}"",'
+  for specialChar in specialChars:
+    txt = txt.replace(specialChar, '')
+  print(txt) 
+  txt = txt.replace(',', ' ')
+  return txt
+
+def monHoroscope(signe):
+    signesZodiaque = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius",
+                      "Capricorn", "Aquarius", "Pisces"]
+    url = f"https://devbrewer-horoscope.p.rapidapi.com/month/short/{signe}"
+    headers = {
+        'x-rapidapi-key': "87a382c5edmsh51597dd88bcc11cp1ae5e8jsn63ef3ac90d64",
+        'x-rapidapi-host': "devbrewer-horoscope.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers)
+    return processString(response.text)
+
+#Créer la commande !signes
+@bot.command()
+async def signes(ctx):
+    '''Permet d'afficher la liste des signes astrologiques pour la commande !horoscope'''
+    await ctx.send("Voici la liste des signes astrologiques que vous pouvez appeler avec la commande !Horoscope: **Aries**, **Taurus**, **Gemini**, **Cancer**, **Leo**, **Virgo**, **Libra**, **Scorpio**, **Sagittarius**, **Capricorn**, **Aquarius**, **Pisces**")
+
+@bot.command()
+async def horoscope(ctx, signe):
+    '''Découvrez l'horoscope du mois selon le signe (vous pouvez avoir la liste de ces derniers avec la commande !signes) grâce à la commande !horoscope *votreSigne*'''
+    await ctx.send(monHoroscope(signe))
+
+@bot.command()
+#Cette fonction permet de comparer deux personnes grâce à la commande !comp *personne 1* *personne 2*
+async def comp(ctx, nouveau_membre1: discord.Member, nouveau_membre2: discord.Member):
+    '''Permet de tester la compatibilité entre deux membre du discord grâce à la commande **!comp @LePseudo1 @LePseudo2**'''
+    pseudo1 = nouveau_membre1.mention
+    pseudo2= nouveau_membre2.mention
+    if pseudo1 == pseudo2:
+        await ctx.send("Il faut que vous indiquiez deux personnes différentes !")
+    else:
+        compatibilite = randint(0, 100)
+        await ctx.send(f" D'après ma boule de cristal {pseudo1} et {pseudo2} sont à {compatibilite} % compatbiles !")
+        if compatibilite <= 40:
+            gifMechant = ["https://tenor.com/view/baby-angry-eating-mad-grumpy-gif-14328548", "https://tenor.com/view/baby-baby-boy-angry-gif-12326628", "https://tenor.com/view/brat-mad-upset-tantrum-badkid-gif-5945754", "https://media1.giphy.com/media/VI2jp6o8ojW53xmupr/giphy.gif?cid=ecf05e47akqk5rm5pfzyczewmyys6ltzfib7if3xff0j0i8z&rid=giphy.gif"]
+            await ctx.send(f"{gifMechant[randint(0, len(gifMechant))]}")
+        if compatibilite >= 41 and compatibilite <= 70:
+            gifMoyen = ["https://tenor.com/view/idk-i-dont-know-sebastian-stan-lol-wtf-gif-5364867", "https://tenor.com/view/idk-not-me-innocent-gif-5742406", "https://tenor.com/view/confused-fresh-prince-will-smith-gif-5207985", "https://tenor.com/view/stevenyeun-idk-meh-eh-shrug-gif-5140003"]
+            await ctx.send(f"{gifMoyen[randint(0, len(gifMoyen) - 1)]}")
+        if compatibilite >= 71:
+            gifHeureux = ["https://tenor.com/view/running-hug-embrace-i-miss-you-good-to-see-you-again-gif-15965620", "https://tenor.com/view/claire-dancing-baby-sunglasses-toddler-gif-11410651", "https://tenor.com/view/-gif-4270352", "https://tenor.com/view/hug-best-friend-one-tree-hill-peyton-sawyer-brooke-davis-gif-15326502", "https://tenor.com/view/spongebob-cartoon-friends-funny-imissyou-gif-3523153"]
+            await ctx.send(f"{gifHeureux[randint(0, len(gifHeureux) - 1)]}")
+# détecter quand quelqu'un ajoute un emoji sur un message
+@bot.event
+async def on_raw_reaction_add(payload):
+
+    emoji = payload.emoji  # recupere l'emoji
+    canal = payload.channel_id  # recupere le numero du canal
+    message = payload.message_id  # recupere le numero du message
+
+    Dog_role = get(bot.get_guild(payload.guild_id).roles, name="🎨Artiste🎨")
+    membre = await bot.get_guild(payload.guild_id).fetch_member(payload.user_id)
+    print(emoji.name)
+    # verifier si l'emoji qu'on a ajoutée est "Capture"
+    if canal == 826810614168551435 and message == 826812016303276034 and emoji.name == "artiste":
+        print("Grade ajouté !")
+        await membre.add_roles(Dog_role)
+        await membre.send("Tu obtiens le grade Artiste !")
+
+
+# détecter quand quelqu'un ajoute un emoji sur un message
+@bot.event
+async def on_raw_reaction_remove(payload):
+
+    emoji = payload.emoji  # recupere l'emoji
+    canal = payload.channel_id  # recupere le numero du canal
+    message = payload.message_id # recupere le numero du messae
+
+    Dog_role = get(bot.get_guild(payload.guild_id).roles, name="🎨Artiste🎨")
+    membre = await bot.get_guild(payload.guild_id).fetch_member(payload.user_id)
+
+    # verifier si l'emoji qu'on a ajoutée est "Capture"
+    if canal == 826810614168551435 and message == 826812016303276034 and emoji.name == "artiste":
+        print("Grade supprimé !")
+        await membre.remove_roles(Dog_role)
+        await membre.send("Tu perds le grade Artiste !")
+
+# phrase
+print("Lancement de DAVY...")
+
+token = open("token.txt", "r").readline()
+# connecter au serveur
+bot.run(token)
